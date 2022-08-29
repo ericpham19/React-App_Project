2aa3cd33276ec48b9f422c0c4e5bbf464a6d22c7
Duy Pham <ericpham19@outlook.com>
2022-08-29 12:26:44 +1000

    added routes and css

---
+++ src/Pages/Details.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Details = () => {
+  return (
+    <div>
+      Coins Details
+    </div>
+  )
+}
+
+export default Details
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      Crypto App React
+    </div>
+  )
+}
+
+export default Home
