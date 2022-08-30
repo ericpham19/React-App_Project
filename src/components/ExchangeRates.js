7b15d936bf06062fa09426c93785f93d29ddae5b
Duy Pham <ericpham19@outlook.com>
2022-08-30 15:28:07 +1000

    adjusted styling and added crypto details

--- src/components/ExchangeRates.js
+++ src/components/ExchangeRates.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import {useState} from "react";
 
+const ExchangeRates = ({ pricechange, name, marketcap, price, symbol, image }) => {
 
-const ExchangeRates = ({name}) => {
   return (
-    <div className="font-light mb-2 p-2 border-red-200 border-2 border-solid rounded hover:bg-blue-200">
-        <div className="flex items-center gap-1">
-          
-         
+    <div className=" h-20 font-light ml-2 mb-2 p-2 border-red-200 border-2 border-solid rounded hover:bg-blue-200 flex    ">
+      <div className="mt-4 flex gap-4">
+        <p>${price}</p>
+        <p className= { {pricechange} >= 0 ? "positive-price-change" : "negative-price-change"}>{pricechange}%</p>
+    
+      </div>
+      <div className="flex items-center gap-2 justify-center absolute  ml-20">
+        <div className="w-1/12 ">
+          <img src={image} alt={name} />
+        </div>
+        <div className="flex gap-1">
           <p>{name}</p>
-          
+          <p className="uppercase">({symbol})</p>
+        </div>
+        <div className= "ml-5">
+          <p>Market Cap:{marketcap}</p>
         </div>
       </div>
-  )
-}
+    </div>
+  );
+};
 
 export default ExchangeRates;
--- src/components/TrendingCoin.js
+++ src/components/TrendingCoin.js
@@ -3,11 +3,18 @@ import React from 'react'
 const TrendingCoin = ({coin}) => {
     
   return (
-    <div className="font-light mb-2 p-2 border-red-200 border-2 border-solid rounded hover:bg-blue-200">
-        <div className="flex items-center gap-1">
+    <div className="font-light h-20 mr-2 mb-2 p-2 border-red-200 border-2 border-solid rounded hover:bg-blue-200">
+        <div className="flex items-center gap-2 absolute">
+          <span>{coin.score +1}</span>
           
+          <div className= "w-1/12">
+          <img src={coin.large}></img>
+          </div>
          
           <p>{coin.name}</p>
+          <p>({coin.symbol})</p>
+          <p className="text-end">Market Rank:{coin.market_cap_rank}</p>
+          
           
         </div>
       </div>
